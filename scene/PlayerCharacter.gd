41a27418937fa95919229ca4afc6134295da407f
Bernardo Villarinho <bervillarinho@gmail.com>
2018-12-01 15:46:00 -0200

    first commit

---
+++ scene/PlayerCharacter.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+export var motion = Vector2()
+export var speed = 250
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("ui_cancel"):
+		speed = 500
+	if Input.is_action_just_released("ui_cancel"):
+		speed = 250
+	if Input.is_action_pressed("ui_up"):
+		motion.y = -speed
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = speed
+	else:
+		motion.y = 0
+	if Input.is_action_pressed("ui_left"):
+		motion.x = -speed
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = speed
+	else:
+		motion.x = 0
+	motion = move_and_slide(motion)
